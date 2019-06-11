8cb7cc9467ae5968a93f3596e47d85f30f6cc07a
Tim Sakow <tcsakow@gmail.com>
2019-06-11 11:58:37 -0400

    Add skeleton set up for js

---
+++ baseball_visuals/field_map.js
@@ -0,0 +1,15 @@
+
+const width = 600;
+const height = 600;
+
+const padding = {
+    top: 20,
+    right: 20,
+    bottom: 40,
+    left: 50,
+};
+
+const plotAreaWidth = width - padding.left - padding.right;
+const plotAreaHeight = height - padding.top - padding.bottom;
+
+const pointRadius = 3;
